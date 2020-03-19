aec4d07326e33e3b369aa257cd5800fe66b6169b
Jason <jason_skillman@outlook.com>
2020-03-19 09:11:38 -0400

    Add project files.

---
+++ Weapon Decorator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon_Decorator {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
