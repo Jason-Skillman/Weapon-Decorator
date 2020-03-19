b24aa6aaa795a22ea3a337e22c72ac112a915f45
Jason <jason_skillman@outlook.com>
2020-03-19 09:32:45 -0400

    Created weapons

---
+++ Weapon Decorator/Weapons/Hammer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon_Decorator {
+
+    public class Hammer : Weapon {
+
+        public override int GetDamage() {
+            return 2;
+        }
+
+        public override string GetName() {
+            return "Hammer";
+        }
+
+    }
+
+}
---
+++ Weapon Decorator/Weapons/Sword.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon_Decorator {
+
+    public class Sword : Weapon {
+
+        public override int GetDamage() {
+            return 1;
+        }
+
+        public override string GetName() {
+            return "Sword";
+        }
+
+    }
+
+}
