feabb9e3ee160d3ef6779af62c33e49eb89aad3f
Jason <jason_skillman@outlook.com>
2020-03-19 09:41:41 -0400

    Created driver class

---
+++ Weapon Decorator/Driver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon_Decorator {
+    class Driver {
+        static void Main(string[] args) {
+
+            Weapon weaponSword = new Sword();
+            weaponSword = new Fire(weaponSword);
+            weaponSword = new Ice(weaponSword);
+            weaponSword = new Lighting(weaponSword);
+
+            Weapon weaponBow = new Bow();
+
+            Weapon weaponHammer = new Hammer();
+            weaponHammer = new Lighting(weaponHammer);
+
+
+
+            Console.WriteLine("Weapon 1: " + weaponSword.GetName());
+            Console.WriteLine("Damage: " + weaponSword.GetDamage());
+
+            Console.WriteLine("Weapon 2: " + weaponBow.GetName());
+            Console.WriteLine("Damage: " + weaponBow.GetDamage());
+
+            Console.WriteLine("Weapon 3: " + weaponHammer.GetName());
+            Console.WriteLine("Damage: " + weaponHammer.GetDamage());
+
+            Console.ReadLine();
+        }
+    }
+}
