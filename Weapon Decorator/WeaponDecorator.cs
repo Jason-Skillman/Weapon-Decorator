3303b8c3b9477b9696152777489069ffa1abc2ff
Jason <jason_skillman@outlook.com>
2020-03-19 09:29:46 -0400

    Created WeaponDecorator class

---
+++ Weapon Decorator/WeaponDecorator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon_Decorator {
+
+    public abstract class WeaponDecorator : Weapon {
+
+        protected Weapon Weapon {
+            get; private set;
+        }
+
+
+        public WeaponDecorator(Weapon weapon) {
+            Weapon = weapon;
+        }
+        
+    }
+
+}
